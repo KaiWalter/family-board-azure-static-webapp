4a53188f2ba8e158da50a454b51e509034800986
Kai Walter <kai.walter@outlook.de>
2020-10-04 13:34:31 +0200

    try store to blob

---
+++ api/BoardConfiguration.cs
@@ -0,0 +1,8 @@
+
+namespace api
+{
+    public class BoardConfiguration
+    {
+        public string MSAToken { get; set; }
+    }
+}
--- api/health.cs
+++ api/health.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.Storage;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -15,6 +16,7 @@ namespace api
         [FunctionName("health")]
         public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            [Blob("familyboard/configuration.json", FileAccess.Write)] out string configuration,
             ILogger log)
         {
             log.LogInformation("Health request.");
@@ -26,6 +28,11 @@ namespace api
                 Status = "Healthy.",
                 Claim = claim?.Identity?.Name ?? "none",
             };
+
+            configuration = JsonConvert.SerializeObject(new BoardConfiguration()
+            {
+                MSAToken = "test",
+            }, Formatting.None);
 
             return new OkObjectResult(responseMessage);
         }
